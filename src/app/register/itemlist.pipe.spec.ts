063538794ca39699f024dfa4bdf81cc0ff68ac80
Sathira <sathiraumesh@gmail.com>
2018-02-16 01:57:22 +0530

    fixed issue 29 added relavant filtering

---
+++ src/app/register/itemlist.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ItemlistPipe } from './itemlist.pipe';
+
+describe('ItemlistPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ItemlistPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,6 +1,6 @@
 export class Register {
 
-    constructor(public firstName:string,public lastName:string,public birthDay:string,public gender:string,public password:string,public email:string,public confirmPassword:string,public username:string,public university:string,public faculty:string,public department:string){
+    constructor(public firstName:string,public lastName:string,public birthDay:string,public gender:string,public password:string,public email:string,public confirmPassword:string,public username:string,public faculty:string,public department:string,public degree:string){
 
 
     }
