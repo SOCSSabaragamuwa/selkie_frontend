4962f8edd6459320bc42df269017ad8a30af21af
Sathira Umesh <sathiraumesh@gmail.com>
2018-02-19 15:27:56 +0530

    fixed the form errors (#57)

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,6 +1,6 @@
 export class Register {
 
-    constructor(public firstName:string,public lastName:string,public birthDay:string,public gender:string,public password:string,public email:string,public confirmPassword:string,public username:string,public faculty:string,public department:string,public degree:string){
+    constructor(public firstName:string,public lastName:string,public birthDay:string,public role:string,public password:string,public email:string,public confirmPassword:string,public username:string,public faculty:string,public department:string,public degree:string){
 
 
     }
