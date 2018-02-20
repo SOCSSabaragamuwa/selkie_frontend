c9c39010c107496b8305469e5e6e9a606d93f217
Sathira <sathiraumesh@gmail.com>
2018-02-20 12:28:31 +0530

    FRecreate

--- src/app/events/event.service.ts
+++ src/app/events/event.service.ts
@@ -9,8 +9,9 @@ export class EventService {
     eventLocation:"Sabaragamuwa university of Srilanka",
     date:"24/2/2018",
     time:"9.00am",
-    longDes:"Platform hackthon",
-    shortDes:"unique hacthon"
+    description:"Platform hackthon",
+    start_at:""
+  
     },
      {
       id:2,
@@ -18,8 +19,9 @@ export class EventService {
       eventLocation:"Sabaragamuwa university of Srilanka",
       date:"9/5/2018",
       time:"6.00pm",
-      longDes:"Introduction to new tech and ha a soft skill sesion",
-      shortDes:"second fort night"
+      description:"Introduction to new tech and ha a soft skill sesion",
+      start_at:""
+     
      },
 
     // {eventName:"EventThree"}
--- src/app/events/eventobj.ts
+++ src/app/events/eventobj.ts
@@ -1,6 +1,6 @@
 export class Eventobj {
 
-    constructor(public id:number,public eventName:string,public eventLocation:string,public date:string,public time:string,public longDes:string,public shortDes:string){
+    constructor(public id:number,public eventName:string,public eventLocation:string,public date:string,public time:string,public description:string,public start_at:string){
 
 
     }
