e8f1264762d973b0ad069e77f6beeb856bf6d573
Sathira Umesh <sathiraumesh@gmail.com>
2018-02-23 10:50:18 +0530

    Fixes:#102 fixed the responsively of admin home page (#103)
    * created the user profile list using two routes
    
    * made the home page responsive

--- src/app/userprofile/user.routes.ts
+++ src/app/userprofile/user.routes.ts
@@ -6,7 +6,9 @@ import { UsereditComponent } from "./useredit.component";
 
 export const  UserRouting: Routes = [
     { path:'',component: UserprofileComponent},
+
     { path:':id',component: UserprofileComponent},
+
     { path: 'edit/:id', component:UsereditComponent },
     // { path: ':id', component:UserprofileComponent },
   
